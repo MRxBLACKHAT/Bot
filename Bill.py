f5e1006a1f792f35a1391392ac90dbea810e9f21
Mr. Blackhat <95309676+MRxBLACKHAT@users.noreply.github.com>
2026-05-26 02:34:52 +0530

    Bill.py

--- Bill.py
+++ Bill.py
@@ -353,5 +353,18 @@ def total_earned(message):
     )
 
 print("🔥 Bot is running...")
+from flask import Flask
+from threading import Thread
 
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+t = Thread(target=run)
+t.start()
 bot.infinity_polling()
